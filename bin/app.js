946eacf0624fd1d06e946a3f77841a34e8d39119
sduduzog <gumedesduduzo@gmail.com>
2020-09-11 21:21:32 +0200

    :alembic: compatibility

--- bin/app.js
+++ bin/app.js
@@ -4,6 +4,7 @@ const child_process_1 = require("child_process");
 const util_1 = require("util");
 const execAsync = util_1.promisify(child_process_1.exec);
 async function start() {
+    await execAsync("ls -a");
     const result = await execAsync("git branch");
     console.log(result);
 }
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { exec } from "child_process";
 import { promisify } from "util";
 const execAsync = promisify(exec);
 export async function start() {
+  await execAsync("ls -a");
   const result = await execAsync("git branch");
   console.log(result);
 }
