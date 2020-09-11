3bc96b912e1ed85cddbcaf7ea6add351336573fa
sduduzog <gumedesduduzo@gmail.com>
2020-09-11 21:42:31 +0200

    changed platform

--- bin/index.js
+++ bin/index.js
@@ -2,3 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const os_1 = require("os");
 console.log(os_1.type());
+console.log(os_1.platform());
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import { type } from "os";
+import { type, platform } from "os";
 import { start } from "./app";
 
 console.log(type());
+console.log(platform());
