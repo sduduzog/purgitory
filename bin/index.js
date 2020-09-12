fbeb37f352e1cc523f51ea157d48bfc756306309
sduduzog <gumedesduduzo@gmail.com>
2020-09-12 17:07:39 +0200

    remove console.log

--- bin/index.js
+++ bin/index.js
@@ -5,11 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 const yargs_1 = __importDefault(require("yargs"));
-const args = yargs_1.default.usage("Usage: $0 [options]").option("dry-run", {
+const args = yargs_1.default.usage("Usage: purgit [options]").option("dry-run", {
     alias: "d",
     type: "boolean",
     default: false,
     description: "A rehearsal, no side effects",
 }).argv;
-console.log("fooooo", args);
 app_1.start(args);
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,11 @@
 import { start } from "./app";
 import yargs from "yargs";
 
-const args = yargs.usage("Usage: $0 [options]").option("dry-run", {
+const args = yargs.usage("Usage: purgit [options]").option("dry-run", {
   alias: "d",
   type: "boolean",
   default: false,
   description: "A rehearsal, no side effects",
 }).argv;
 
-console.log("fooooo", args);
 start(args);
