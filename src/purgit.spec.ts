ae5b31e5240db86b4010d3c05991b6e95d02e4e3
sduduzog <gumedesduduzo@gmail.com>
2020-09-20 02:10:44 +0200

    more cleanup

--- src/purgit.spec.ts
+++ src/purgit.spec.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const mockApp = {
-  start: jest.fn().mockResolvedValue(Promise.resolve("")),
-  dryRunStart: jest.fn().mockResolvedValue(Promise.resolve("")),
+  start: jest.fn().mockResolvedValue(Promise.resolve()),
+  dryRunStart: jest.fn().mockResolvedValue(Promise.resolve()),
 };
 
 describe(`purgit.js`, () => {
